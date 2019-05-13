ff68027e18bf16ef0fdb47fcc33c8b08f8c5db21
AliGangeh <40417826+AliGangeh@users.noreply.github.com>
2019-05-12 19:44:17 -0700

    can classify mnist data set with 98% accuracy

--- MNIST_ConvolutionalNN.py
+++ MNIST_ConvolutionalNN.py
@@ -6,7 +6,13 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
 from keras.utils.np_utils import to_categorical
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
 import random
+import cv2
+import requests
+from PIL import Image
 
 np.random.seed(0)
 (X_train, y_train), (X_test, y_test)= mnist.load_data()
@@ -19,10 +25,8 @@ assert(X_test.shape[0] == y_test.shape[0]), "The number of images is not equal t
 assert(X_test.shape[1:] == (28,28)), "The dimensions of the images are not 28 x 28."
 
 num_of_samples=[]
-
 cols = 5
 num_classes = 10
-
 fig, axs = plt.subplots(nrows=num_classes, ncols=cols, figsize=(5,10))
 fig.tight_layout()
 
@@ -51,3 +55,67 @@ y_test = to_categorical(y_test, 10)
 
 X_train = X_train/255
 X_test = X_test/255
+
+#definition of leNet_model function
+def leNet_model():
+  model = Sequential()
+  model.add(Conv2D(30, (5,5), input_shape=(28, 28, 1), activation="relu"))
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+  model.add(Conv2D(15, (3, 3), activation="relu"))
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+  model.add(Flatten())
+  model.add(Dense(500, activation="relu"))
+  model.add(Dense(num_classes, activation="softmax"))
+  model.compile(Adam(lr=0.01), loss="categorical_crossentropy", metrics=["accuracy"])
+  return model
+
+model = leNet_model()
+print(model.summary())
+
+history = model.fit(X_train, y_train, epochs=10, validation_split=0.1, batch_size=400, verbose=1, shuffle=1)
+
+#plots loss
+plt.plot(history.history["loss"])
+plt.plot(history.history["val_loss"])
+plt.legend(["loss", "val_loss"])
+plt.title("loss")
+plt.xlabel("epochs")
+plt.show()
+
+#plots accuracy
+plt.plot(history.history["acc"])
+plt.plot(history.history["val_acc"])
+plt.legend(["acc", "val_acc"])
+plt.title("accuracy")
+plt.xlabel("epochs")
+plt.show()
+
+#tests generalization to see how good it is with data it wasn't trained with it has 92.88% accuracy
+score= model.evaluate(X_test, y_test, verbose=0)
+print(type(score))
+print("test score:", score[0])
+print("test accuracy", score[1])
+
+#imports image and makes it usable for the model
+url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST8KzXHtkSHcxzdpnllMhAj0upLEwnNFdtY6j4YUPcmaf4Ty3u'
+response = requests.get(url, stream=True)
+img = Image.open(response.raw)
+plt.imshow(img, cmap=plt.get_cmap("gray"))
+
+img = np.asarray(img)
+img = cv2.resize(img, (28, 28))
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.bitwise_not(img)
+plt.imshow(img, cmap=plt.get_cmap('gray'))
+plt.show()
+
+img = img/255
+img = img.reshape(1, 28, 28, 1)
+
+prediction = model.predict_classes(img)
+print("predicted digit:", str(prediction))
+
+score= model.evaluate(X_test, y_test, verbose=0)
+print(type(score))
+print("test score:", score[0])
+print("test accuracy", score[1])
